474cefd054ecaf82980e8025823b96125133d360
Ammar Hassan <ammar.hassan@hagridsolutions.com>
2019-05-07 12:00:36 +1000

    Complete App

---
+++ src/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+    domain: "dev-c0ooqk5h.au.auth0.com",
+    roleUrl: "https://rbac-tutorial-app/role",
+    clientId: "tCNSLgx3Mi0gwCx03YjaHNS0v64VdyKa",
+    callbackUrl: "http://localhost:3000/callback"
+  };
